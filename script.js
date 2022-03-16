894fcd142e4871465fd5ab7e3898d37310047a18
saleh23hayati <shayati2003.uk@gmail.com>
2022-03-16 17:50:16 +0000

    done

--- script.js
+++ script.js
@@ -13,3 +13,51 @@ alert (`Saleh Hussain`)
  console.log (Budget >= 30 && Budget <= 80);
  Budget += 30
  console.log (Budget >= 30 && Budget <= 80);
+
+
+
+
+let grade = prompt (`ماهي درجتكك ؟`)
+if (grade > 100){
+    console.log(`حدك قوي`)   
+       
+}
+else if (grade >= 90){
+     console.log(`لقد حصلت على امتياز 🥳`)
+ } else if (grade >= 80){
+    console.log(` لقد حصلت على جيد جداً🤩`)
+} else if (grade >= 70){
+    console.log(` لقد حصلت على جيد جداً🤩`) 
+} else if (grade >= 60){
+    console.log(` لقد حصلت على مقبول😕`) 
+} else if (grade >= 50){
+    console.log(` لقد حصلت على ضعيف☹️`)   
+} else if (grade < 50){
+    console.log(`راسب💔`)   
+       
+}
+// switch (grade) {
+//     case grade >= 90 :
+//     console.log(`لقد حصلت على امتياز 🥳`)
+//         break;
+//     case grade >= 80:
+//         console.log(` لقد حصلت على جيد جداً🤩`)
+//     break;
+//     case grade >= 70: 
+//     console.log(`لقد حصلت على جيد جداً🤩`)
+//         break;
+//         case grade >= 60: 
+//         console.log(` لقد حصلت على مقبول😕`) 
+//         break;
+//         case grade >= 50: 
+//         console.log(` لقد حصلت على ضعيف☹️`)   
+//         break;
+//         case grade > 50: 
+//         console.log(`راسب💔`)   
+//         break;
+
+    
+//     default:  
+
+//         break;
+// }
