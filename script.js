b3bf9e386d4db7257ec3a6b9b6efbab95419c1d5
saleh23hayati <shayati2003.uk@gmail.com>
2022-03-16 16:31:21 +0000

    done

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ alert (`Saleh Hussain`)
  console. log(18);
  console. log(`Im in the UK`);
  console.log('%c javascript! ', 'background: #222; color: #bada55');
+
+
+
+
+
+
+
+ let Budget = 20
+ console.log (Budget >= 30 && Budget <= 80);
+ Budget = +30
+ console.log (Budget >= 30 && Budget <= 80);
